b26d842d351cdb5a81e8d2d7c266b92cf1fd689a
romanolivi <romano_livi@outlook.com>
2021-03-23 16:59:06 -0400

    commit tree 3c2954dd0058da3e72e151b4df9e3f7db1ad8b64

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 16;
+let num4 = 6;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.random() + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 4, 6, 20);
